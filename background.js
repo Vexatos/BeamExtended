e76d4c207afbc3755e9d3ff44cb9ed62bb2f2445
Anthony Martin <idioticdinner@gmail.com>
2015-03-18 01:30:01 -0700

    Add bexbadges to the list.
    This might make options stop resetting. Did you forget it or is there a reason you didn't add it?

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.storage.sync.get("options", function(items) {
-	if(Object.keys(items).length != 4) {
-		chrome.storage.sync.set({options: {twitchemotes: true, linkimages: false, usercolors: true, twitchbadges: false}});
+	if(Object.keys(items).length != 5) {
+		chrome.storage.sync.set({options: {twitchemotes: true, linkimages: false, usercolors: true, twitchbadges: false, bexbadges: false}});
 	}
 });
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
