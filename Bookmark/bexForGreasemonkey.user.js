e653590df0ed01ac21b6cb884166d6a5f16797a9
Vexatos <Vexatos@users.noreply.github.com>
2015-03-29 10:32:18 +0200

    Update bexForGreasemonkey.user.js

--- Bookmark/bexForGreasemonkey.user.js
+++ Bookmark/bexForGreasemonkey.user.js
@@ -2,11 +2,11 @@
 // @name        Beam Extended
 // @namespace   http://beamalerts.com/bex/
 // @description This extension will expand the potential of the smaller channels on beam.pro. Offers custom channel emotes, supports all twitch emotes + our own custom emotes, twitch/custom badges and changing direct image links into images. 
-// @author      Vexatos / ExuDev
+// @author      Vexatos / IFDevelopment
 // @homepage    http://beamalerts.com/bex/
-// @icon        https://raw.githubusercontent.com/ExuDev/BeamExtended/master/icons/icon.png
-// @downloadURL https://gist.github.com/Vexatos/ef21f01366a9c8c5630c/raw/105aceeee2f1528e6b7802f0744acc3d6cb80c7e/bexForGreasemonkey.user.js
-// @updateURL   https://gist.github.com/Vexatos/ef21f01366a9c8c5630c/raw/105aceeee2f1528e6b7802f0744acc3d6cb80c7e/bexForGreasemonkey.user.js
+// @icon        https://raw.githubusercontent.com/IFDevelopment/BeamExtended/master/icons/icon.png
+// @downloadURL https://raw.githubusercontent.com/Vexatos/BeamExtended/master/Bookmark/bexForGreasemonkey.user.js
+// @updateURL   https://raw.githubusercontent.com/Vexatos/BeamExtended/master/Bookmark/bexForGreasemonkey.user.js
 // @encoding    utf-8
 // @include     http*://*beam.pro*
 // @version     1.0.0
